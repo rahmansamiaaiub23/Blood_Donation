dbb28bce911a28e23f35ee68c23e2a90695ed0f1
Samia Rahman <samiarahmanaiub23@gmail.com>
2026-07-24 16:19:21 +0600

    repo created

---
+++ Blood_Donation/Blood_Donation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Blood_Donation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
